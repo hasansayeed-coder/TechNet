2cfd00af5e6fb4a61f487258dd5c1329e4f39cf7
hasansayeed-coder <hasansayeed791@gmail.com>
2024-05-13 16:12:14 +0600

    Halfway

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -13,13 +13,15 @@ import {
 } from 'react-icons/hi';
 import { Button } from './ui/button';
 import { IProduct } from '@/types/globalTypes';
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import { addToCart, removeFromCart, removeOne } from '@/redux/features/cart/cartSlice';
 
 export default function Cart() {
   //! Dummy data
 
-  const products: IProduct[] = [];
-  const total = 0;
+  const dispatch = useAppDispatch() ;
 
+  const {products , total} = useAppSelector(state => state.cart)
   //! **
 
   return (
@@ -52,17 +54,17 @@ export default function Cart() {
                 </p>
               </div>
               <div className="border-l pl-5 flex flex-col justify-between">
-                <Button>
+                <Button onClick={() => dispatch(addToCart(product))}>
                   <HiOutlinePlus size="20" />
                 </Button>
-                <Button>
+                <Button onClick={() => dispatch(removeOne(product))}>
                   <HiMinus size="20" />
                 </Button>
                 <Button
                   variant="destructive"
                   className="bg-red-500 hover:bg-red-400"
                 >
-                  <HiOutlineTrash size="20" />
+                  <HiOutlineTrash onClick ={() => dispatch(removeFromCart(product))} size="20" />
                 </Button>
               </div>
             </div>
